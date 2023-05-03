2c803fc7ec4bad2d6e8a3d0bd33ff3910ce82184
Re-zan <zannat881all@gmail.com>
2023-05-03 12:33:07 +0600

    react react-router firebase install done

---
+++ src/utilities/firebase/firebaseConfig.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCG6OalzaWKgzMTftKO9nGNeiBiDIu3230",
+  authDomain: "rezancuisine.firebaseapp.com",
+  projectId: "rezancuisine",
+  storageBucket: "rezancuisine.appspot.com",
+  messagingSenderId: "264775266080",
+  appId: "1:264775266080:web:33fd34d993ab8836317145",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
